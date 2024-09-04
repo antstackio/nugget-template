988d30763e6677a90b01fbbc8463d6350487ac99
Akshatha Laxmi <akshata.lakshmi@gmail.com>
2024-09-04 12:16:14 +0530

    initial commit

---
+++ {{cookicutter.project_name}}/src/notes/index.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+import notes from "./notes-module";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        const note = await notes.getItem("1");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                note: JSON.stringify(note),
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
---
+++ {{cookicutter.project_name}}/src/users/index.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import users from './users-module';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        console.log(process.env.NODE_PATH)
+        const user = await users.getUser(1);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `hello ${user ? user.username : 'user'}!`,
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
